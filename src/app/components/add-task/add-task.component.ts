b0d023e6f8734f67bf5f34687de759269729b7f6
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-03-20 18:11:36 -0400

    feat: add functionality for close and add button in header

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -13,8 +15,12 @@ export class AddTaskComponent implements OnInit {
   reminder: boolean = false;
   errorMessageText: string = '';
   errorMessageDay: string = '';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor( private uiService: UiService ) {
+    this.subscription = this.uiService.onToggle().subscribe((value) =>  this.showAddTask = value )
+  }
 
   ngOnInit(): void {}
 
